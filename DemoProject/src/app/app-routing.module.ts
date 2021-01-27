6877ea4e15406d214b01df6f628d6fc0bd16b48b
Abhishek Suthar <62384044+abhisheksuthar2848@users.noreply.github.com>
2021-01-28 03:39:54 +0530

    Revert "Revert "blogger add""
    This reverts commit f5c26af0ebb4688ad505e8a25b7d37b62f015ce8.

--- DemoProject/src/app/app-routing.module.ts
+++ DemoProject/src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'products',component:ProductListComponent},
+  {path:'product/:id',component:ProductDetailComponent},
+  {path:'',redirectTo:'/products',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes),CommonModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+ }
